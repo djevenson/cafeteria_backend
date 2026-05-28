cdb9a43bd326eb3ae0a3467fc403bee60698b123
Djevenson <djivensjanvier@gmail.com>
2026-05-28 08:59:48 -0400

    Ajout des endpoint pour categories

---
+++ app/routers/categories.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Form, HTTPException
+from app.database import get_connection
+
+router=APIRouter()
+
+@router.post("/categories")
+def set_categories(cat_name:str):
+    connection=get_connection()
+    cursor=connection.cursor()
+    cursor.execute(
+        """
+        INSERT INTO categories(category_name) VALUES (%s) RETURNING *
+        """,
+        (cat_name,)
+    )
+    categorie=cursor.fetchone()
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return {"message":"categorie added successfully!!", "categorie":f"{categorie}"}
+
+
+@router.get("/categories")
+def set_categories(cat_id:str):
+    connection=get_connection()
+    cursor=connection.cursor()
+    cursor.execute(
+        """
+        SELECT * FROM categories WHERE category_id=%s 
+        """,
+        (cat_id,)
+    )
+    categorie=cursor.fetchone()
+    cursor.execute()
+    cursor.close()
+    connection.close()
+
+    return categorie
